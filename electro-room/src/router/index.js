abbdb31de4464b8af145f28955aa31377d590a42
Arsen Cenollari <arsencenollari89910@gmail.com>
2023-10-02 21:46:44 +0200

    fixed axios and sass

--- electro-room/src/router/index.js
+++ electro-room/src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home-vue',
       name: 'home',
       component: HomeView
     },
--- electro-room/src/views/HomeView.vue
+++ electro-room/src/views/HomeView.vue
@@ -1,18 +1,52 @@
 <template>
-  <v-card>
-    <v-layout>
-      <v-navigation-drawer
-        image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
-        permanent
-        theme="dark"
-      >
-        <v-list nav>
-          <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors" value="supervisors"></v-list-item>
-          <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-      <v-main style="height: 250px"></v-main>
-    </v-layout>
-  </v-card>
+  <div>
+    <div v-if="loading">Loading...</div>
+    <div v-else class="container is-fluid">
+      <div class="tile is-ancestor">
+        <Tiles v-for="idea in ideas" :key="idea._id" :id="idea._id">
+          <!-- <template slot="title">{{ idea.title }}</template>
+          <template slot="description">{{ idea.details }}</template> -->
+        </Tiles>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import axios from 'axios';
+import Tiles from '@/components/Tiles/Tiles.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    Tiles,
+  },
+  data() {
+    return {
+      ideas: [],
+      loading: false,
+    };
+  },
+  created() {
+    this.fetchNotes();
+  },
+  methods: {
+    fetchNotes() {
+      this.loading = true;
+      axios.get('https://protected-fjord-22747.herokuapp.com/ideas')
+        .then(({ data: { ideas } }) => {
+          this.ideas = ideas;
+          this.loading = false;
+        })
+        .catch(error => console.log(error));
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+  
+.container {
+  padding-top: 50px;
+}
+
+</style>
